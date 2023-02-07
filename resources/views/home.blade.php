b4b99efbaee237d656197e15b85553ab90effb1a
francescotrudu <francescotrudu84@gmail.com>
2023-02-07 17:37:33 +0100

    Error non found

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Intro</title>
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 <body>
-    <h1>Laravel Hello World</h1>
+    <header class="container mt-3">
+        <div class="row flex-row justify-content-between">
+            <div class="col-12">
+                <nav class="navbar">
+                    <ul class="navbar-nav d-flex flex-row gap-3">
+                        <a href="href="/welcome"><li class="nav-item">Home</li></a>
+                        <a href=""><li class="nav-item">Documentation</li></a>
+                        <a href=""><li class="nav-item">Contacts</li></a>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="col-12">
+            <h1 class="fs-1 text-center">Laravel from 0 to Zero</h1>
+        </div>
+    </header>
     
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
